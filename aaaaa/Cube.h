c9f39181fbe30a9d7816449dfccc676a1a7bf35b
Aescosaurus <michaelbuslik@hotmail.com>
2020-10-31 20:33:16 -0600

    Cube of power.

--- aaaaa/Cube.h
+++ aaaaa/Cube.h
@@ -1,6 +1,15 @@
 #pragma once
 
+#include "..\Common\DeviceResources.h"
+#include "..\Content\ShaderStructures.h"
+#include "..\Common\StepTimer.h"
+#include <d3d12.h>
+
 class Cube
 {
-
+private:
+	Microsoft::WRL::ComPtr<ID3D12Resource> m_vertexBuffer;
+	Microsoft::WRL::ComPtr<ID3D12Resource> m_indexBuffer;
+	D3D12_VERTEX_BUFFER_VIEW m_vertexBufferView;
+	D3D12_INDEX_BUFFER_VIEW m_indexBufferView;
 };
