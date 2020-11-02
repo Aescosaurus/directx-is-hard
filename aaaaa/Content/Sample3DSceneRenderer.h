e4c89e2c2b7decb75702fa00a8d6044435ac605e
Aescosaurus <michaelbuslik@hotmail.com>
2020-11-01 20:35:40 -0700

    Revert "It no worky :*("
    This reverts commit 885f43a478cd2857c6ae6347dc0b626476840150.

--- aaaaa/Content/Sample3DSceneRenderer.h
+++ aaaaa/Content/Sample3DSceneRenderer.h
@@ -21,7 +21,7 @@ namespace aaaaa
 		void StartTracking();
 		void TrackingUpdate(float positionX);
 		void StopTracking();
-		// bool IsTracking() { return m_tracking; }
+		bool IsTracking() { return m_tracking; }
 
 	private:
 		void LoadState();
@@ -38,23 +38,23 @@ namespace aaaaa
 		Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList>	m_commandList;
 		Microsoft::WRL::ComPtr<ID3D12RootSignature>			m_rootSignature;
 		Microsoft::WRL::ComPtr<ID3D12PipelineState>			m_pipelineState;
-		// Microsoft::WRL::ComPtr<ID3D12DescriptorHeap>		m_cbvHeap;
-		// Microsoft::WRL::ComPtr<ID3D12Resource>				m_vertexBuffer;
-		// Microsoft::WRL::ComPtr<ID3D12Resource>				m_indexBuffer;
-		// Microsoft::WRL::ComPtr<ID3D12Resource>				m_constantBuffer;
-		// ModelViewProjectionConstantBuffer					m_constantBufferData;
-		// UINT8*												m_mappedConstantBuffer;
-		// UINT												m_cbvDescriptorSize;
+		Microsoft::WRL::ComPtr<ID3D12DescriptorHeap>		m_cbvHeap;
+		Microsoft::WRL::ComPtr<ID3D12Resource>				m_vertexBuffer;
+		Microsoft::WRL::ComPtr<ID3D12Resource>				m_indexBuffer;
+		Microsoft::WRL::ComPtr<ID3D12Resource>				m_constantBuffer;
+		ModelViewProjectionConstantBuffer					m_constantBufferData;
+		UINT8*												m_mappedConstantBuffer;
+		UINT												m_cbvDescriptorSize;
 		D3D12_RECT											m_scissorRect;
 		std::vector<byte>									m_vertexShader;
 		std::vector<byte>									m_pixelShader;
-		// D3D12_VERTEX_BUFFER_VIEW							m_vertexBufferView;
-		// D3D12_INDEX_BUFFER_VIEW								m_indexBufferView;
+		D3D12_VERTEX_BUFFER_VIEW							m_vertexBufferView;
+		D3D12_INDEX_BUFFER_VIEW								m_indexBufferView;
 
 		// Variables used with the rendering loop.
 		bool	m_loadingComplete;
-		// float	m_radiansPerSecond;
-		// float	m_angle;
+		float	m_radiansPerSecond;
+		float	m_angle;
 		bool	m_tracking;
 	};
 }
